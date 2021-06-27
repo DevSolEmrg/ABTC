97c6375ed95b03067c940a7c6d1767fa72eb5937
mkjdy <markdy61@gmail.com>
2021-06-27 18:31:35 +0800

    Add mdi font and change scrollbar color

--- resources/js/components/AuthContainer.vue
+++ resources/js/components/AuthContainer.vue
@@ -1,6 +1,6 @@
 <template>
     <el-container style="height: 100vh;">
-        <el-aside style="background-color: #fff;border-right: 1px solid #eee;">
+        <!--<el-aside v-if="side_nav" style="background-color: #fff;border-right: 1px solid #eee;">
             <el-menu :default-openeds="['1', '3']" style="border:none">
             <el-submenu index="1">
                 <template slot="title"><i class="el-icon-message"></i>Navigator One</template>
@@ -48,14 +48,56 @@
                 </el-submenu>
             </el-submenu>
             </el-menu>
+        </el-aside>-->
+        <el-aside v-if="side_nav" style="background-color: #545C64;border-right: 1px solid #eee;">
+            <el-menu
+                style="border:none"
+                default-active="2"
+                class="el-menu-vertical-demo"
+                @open="handleOpen"
+                @close="handleClose"
+                background-color="#545c64"
+                text-color="#fff"
+                active-text-color="#ffd04b"
+            >
+                <el-submenu index="1">
+                    <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>Navigator One</span>
+                    </template>
+                    <el-menu-item-group title="Group One">
+                    <el-menu-item index="1-1">item one</el-menu-item>
+                    <el-menu-item index="1-2">item one</el-menu-item>
+                    </el-menu-item-group>
+                    <el-menu-item-group title="Group Two">
+                    <el-menu-item index="1-3">item three</el-menu-item>
+                    </el-menu-item-group>
+                    <el-submenu index="1-4">
+                    <template slot="title">item four</template>
+                    <el-menu-item index="1-4-1">item one</el-menu-item>
+                    </el-submenu>
+                </el-submenu>
+                <el-menu-item index="2">
+                    <i class="el-icon-menu"></i>
+                    <span>Navigator Two</span>
+                </el-menu-item>
+                <el-menu-item index="3" disabled>
+                    <i class="el-icon-document"></i>
+                    <span>Navigator Three</span>
+                </el-menu-item>
+                <el-menu-item index="4">
+                    <i class="el-icon-setting"></i>
+                    <span>Navigator Four</span>
+                </el-menu-item>
+            </el-menu>
         </el-aside>
         
         <el-container>
             <el-header style="background-color:white;border-bottom: 1px solid #eee; ">
                 <div class="row">
-                    <div class="col-6"><i class="el-icon-s-fold" style="margin-right: 15px"></i> Title</div>
-                    <div class="col-6" style="text-align: right; font-size: 12px">
-                        <el-dropdown>
+                    <div class="col-6 px-1"> <span class="mdi mdi-menu btn" @click="toggleSideNav()"/> Title / sgsd / sdg</div>
+                    <div class="col-6 pl-1" style="text-align: right; font-size: 12px">
+                        <!--<el-dropdown>
                             <i class="el-icon-setting" style="margin-right: 15px"></i>
                             <el-dropdown-menu slot="dropdown">
                             <el-dropdown-item>View</el-dropdown-item>
@@ -63,7 +105,16 @@
                             <el-dropdown-item>Delete</el-dropdown-item>
                             </el-dropdown-menu>
                         </el-dropdown>
-                        <span>Tom</span>
+                        <span>Tom</span>-->
+                        <el-dropdown trigger="click">
+                            <span class="el-dropdown-link">
+                                Username<i class="el-icon-arrow-down el-icon--right"></i>
+                            </span>
+                            <el-dropdown-menu slot="dropdown">
+                                <el-dropdown-item command="a"><span class="mdi mdi-account" /> Profile</el-dropdown-item>
+                                <el-dropdown-item command="a"><span class="mdi mdi-power" /> Logout</el-dropdown-item>
+                            </el-dropdown-menu>
+                        </el-dropdown>
                     </div>
                 </div>
             
@@ -92,13 +143,23 @@ export default {
             address: 'No. 189, Grove St, Los Angeles'
         };
         return {
+            side_nav: true,
             tableData: Array(23).fill(item)
         }
     },
     methods: {
         samplefunct() {
             alert("clicke")
-        }
+        },
+        toggleSideNav() {
+            this.side_nav = !this.side_nav
+            console.log(this.side_nav)
+        },handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
     }
 }
 </script>
@@ -112,5 +173,8 @@ export default {
     
     .el-aside {
         color: #333;
+    }
+    .el-menu .el-menu--inline {
+        background-color: #333;
     }
 </style>
