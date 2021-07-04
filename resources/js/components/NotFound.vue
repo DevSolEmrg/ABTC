49011f21c9ea168702e9ae97f0dab8dda3e56dd4
mkjdy <markdy61@gmail.com>
2021-07-04 10:40:48 +0800

    Layout or change design not found with functionality back history

---
+++ resources/js/components/NotFound.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="notfound-center">
+        <div class="row">
+            <div class="col-12" style="text-align:center">
+                <h1><strong style="color:red">404</strong></h1>
+            </div>
+            <div class="col-12 mb-2" style="text-align:center">
+                <h4>Page Not Found</h4>
+            </div>
+            <div class="col-12" style="text-align:center">
+                <el-button type="danger" icon="el-icon-back" @click="back">BACK</el-button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        back() {
+            history.back();
+        }
+    },
+    mounted() {
+        //alert(location.href)
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .notfound-center {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height:100vh;
+    }
+</style>
