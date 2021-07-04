ea9051db7104e61f6af2129a969c2df5fa741087
mkjdy <markdy61@gmail.com>
2021-07-04 16:56:35 +0800

    Add patient history db table

---
+++ database/migrations/2021_07_04_073107_create_patient_histories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patient_histories', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->text('place');
+            $table->enum('type_of_animal', ['PD', 'PC']);
+            $table->enum('type', ['B', 'NB']);
+            $table->enum('body_parts', ['R FOOT', 'L LEG', 'L ARM', 'L HAND', 'R LEG', 'BUTTOCKS', 'R HAND']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patient_histories');
+    }
+}
