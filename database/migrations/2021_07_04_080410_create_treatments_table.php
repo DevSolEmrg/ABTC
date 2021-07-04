016c7bae8f973abde4c9c0b13e80d0ea290f3586
mkjdy <markdy61@gmail.com>
2021-07-04 16:57:33 +0800

    Add treatment db table

---
+++ database/migrations/2021_07_04_080410_create_treatments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTreatmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('treatments', function (Blueprint $table) {
+            $table->id();
+            $table->enum('category', ['1', '2', '3']);
+            $table->boolean('is_washed')->default(true);
+            $table->date('rig_date');
+            $table->string('route');
+            $table->date('date');
+            $table->text('brand_name');
+            $table->text('remarks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('treatments');
+    }
+}
