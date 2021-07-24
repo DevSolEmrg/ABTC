587cd1fcee88dbcbf9da10c6d237b19aead11666
mkjdy <markdy61@gmail.com>
2021-07-24 09:40:46 +0800

    add default user to seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Str;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,5 +17,14 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
         \App\Models\Patient::factory(123)->create();
+
+        \App\Models\User::create([
+            'name' => 'John Doe Dee',
+            'email' => 'johndoe@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
     }
 }
