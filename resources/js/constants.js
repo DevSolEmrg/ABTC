3c4a385aaad0fbffb083ef57b60c07a0b0bda73c
mkjdy <markdy61@gmail.com>
2021-07-25 11:04:04 +0800

    Add constant birthtdate calculator function

--- resources/js/constants.js
+++ resources/js/constants.js
@@ -22,3 +22,8 @@ export function buildDate(param_date) {
     let date = new Date(param_date).toLocaleDateString().split('/'), /*yr*/ yr = date[2], /*mon*/ mon = date[0] < 10 ? `0${date[0]}` : date[0], /*day*/ day = date[1] < 10 ? `0${date[1]}` : date[1]
     return `${yr}-${mon}-${day}`
 }
+
+export function calAge(dateString) {
+    var birthday = +new Date(dateString);
+    return ~~((Date.now() - birthday) / (31557600000));
+}
