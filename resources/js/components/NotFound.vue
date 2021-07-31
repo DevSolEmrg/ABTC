874aaf7e1a31a884ced04c240efcc871c7b79961
mkjdy <markdy61@gmail.com>
2021-07-31 16:43:52 +0800

    Update page notfound style add image/logo

--- resources/js/components/NotFound.vue
+++ resources/js/components/NotFound.vue
@@ -2,10 +2,15 @@
     <div class="notfound-center">
         <div class="row">
             <div class="col-12" style="text-align:center">
-                <h1><strong style="color:red">404</strong></h1>
+                <el-avatar class="mb-2" :size="300" :src="baseUrl + 'image/user.png'" @error="errorHandler" style="background-color:#FFC230">
+                        <img :src="baseUrl + 'image/user.png'"/>
+                    </el-avatar>
+            </div>
+            <div class="col-12" style="text-align:center">
+                <h1><strong style="color:#FD0001">404</strong></h1>
             </div>
             <div class="col-12 mb-2" style="text-align:center">
-                <h4>Page Not Found {{ currentURL }}</h4>
+                <h5 style="color:grey">Page Not Found {{ currentURL }}</h5>
             </div>
             <div class="col-12" style="text-align:center">
                 <el-button type="danger" icon="el-icon-back" @click="back">BACK</el-button>
@@ -18,7 +23,8 @@
 export default {
     data() {
         return {
-            currentURL: `"URL(${location.href})"`
+            currentURL: `"URL(${location.href})"`,
+            baseUrl: location.origin.concat('/')
         }
     },
     methods: {
@@ -38,5 +44,6 @@ export default {
         justify-content: center;
         align-items: center;
         height:100vh;
+        background-color: #1A1A1D;
     }
 </style>
