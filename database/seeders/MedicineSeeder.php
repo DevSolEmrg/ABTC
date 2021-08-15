bd89a8146ffe7f1aa7e608f5e2791d58be0ce5ed
mkjdy <markdy61@gmail.com>
2021-08-15 21:56:09 +0800

    add back end for medicine db, factory, seeder, model

---
+++ database/seeders/MedicineSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use App\Models\Medicine;
+
+class MedicineSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Medicine::factory()->times(10)->create();
+    }
+}
