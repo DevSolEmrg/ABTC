42918b593feb904d84977b4bda6e9f68a6f1fdef
mkjdy <markdy61@gmail.com>
2021-08-30 16:44:23 +0800

    add seeder for role and permission and assign to default user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 
 use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,13 +23,78 @@ class DatabaseSeeder extends Seeder
         \App\Models\Vaccine::factory(17)->create();
         \App\Models\Personnel::factory(20)->create();
 
-        \App\Models\User::create([
+        $default_user = \App\Models\User::create([
             'name' => 'John Doe Dee',
             'email' => 'johndoe@gmail.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ]);
+
+        $roles = [
+            'super-admin',
+            'physician',
+            'nurse'
+        ];
+
+        $permissions = [
+            //patient
+            'create-patient-record',
+            'read-patient-record',
+            'update-patient-record',
+            'delete-patient-record',
+            'print-patient-record',
+            'generate-patient-report',
+            //personnel
+            'create-personnel',
+            'read-personnel-info',
+            'update-personnel-info',
+            'delete-personnel',
+            //vaccine
+            'create-vaccine',
+            'read-vaccine-info',
+            'update-vaccine-info',
+            'delete-vaccine-info',
+            //user management
+            'create-user-account',
+            'read-user-account-info',
+            'update-user-account',
+            'delete-user-account',
+            //role & permission
+            'create-role',
+            'read-role-info',
+            'update-role',
+            'delete-role'
+        ];
+
+        foreach($permissions as $permit) {
+            Permission::create(['name' => $permit]);
+        }
+
+        foreach($roles as $role_key=>$role) {
+            $role = Role::create(['name' => $role]);
+
+            if ($role_key == 0) {
+                foreach($permissions as $key=>$value) {
+                    $pi = $key + 1;
+                    $permission = Permission::find($pi);
+
+                    $role->givePermissionTo($permission->name);
+                    $permission->assignRole($role->name);
+                }
+            } else {
+                foreach($permissions as $key=>$value) {
+                    $pi = $key + 1;
+                    if ($pi < 7) {
+                        $permission = Permission::find($pi);
+
+                        $role->givePermissionTo($permission->name);
+                        $permission->assignRole($role->name);
+                    }
+                }
+            }
+        }
+        $default_user->assignRole('super-admin');
 
     }
 }
