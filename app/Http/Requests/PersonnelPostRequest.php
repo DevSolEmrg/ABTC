0759b18dbcfd5e64f2ec41a18cfcd2f356be5493
mkjdy <markdy61@gmail.com>
2021-09-07 21:33:13 +0800

    add auth check in post request validation

--- app/Http/Requests/PersonnelPostRequest.php
+++ app/Http/Requests/PersonnelPostRequest.php
@@ -13,7 +13,7 @@ class PersonnelPostRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return auth()->check();
     }
 
     /**
--- app/Http/Requests/UserPostRequest.php
+++ app/Http/Requests/UserPostRequest.php
@@ -13,7 +13,7 @@ class UserPostRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return auth()->check();
     }
 
     /**
