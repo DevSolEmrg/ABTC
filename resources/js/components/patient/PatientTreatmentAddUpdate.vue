e5e58c097c950539cefff12cf941af2be808b933
mkjdy <markdy61@gmail.com>
2021-10-09 23:30:43 +0800

    add exposure form

--- resources/js/components/patient/PatientTreatmentAddUpdate.vue
+++ resources/js/components/patient/PatientTreatmentAddUpdate.vue
@@ -24,63 +24,106 @@
   <div class="text item">
     <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
         <el-tabs type="border-card" class="mb-3">
-            <el-tab-pane label="Patient History">
-                <el-form-item label="Date of Incident" required>
-                    <el-form-item prop="date_of_incident">
-                        <el-date-picker ref="name" type="date" placeholder="Pick a date" v-model="ruleForm.date_of_incident" :picker-options="pickerOptions" timezone="UTC"></el-date-picker>
-                    </el-form-item>
-                </el-form-item>
-                <el-form-item label="Place of Inci." prop="place_of_incident">
-                    <el-input type="textarea" rows="2" v-model="ruleForm.place_of_incident" :clearable="true"></el-input>
-                </el-form-item>
-                <el-form-item label="Date of Exam" required>
-                    <el-form-item prop="date_of_physical_exam">
-                        <el-date-picker type="date" placeholder="Pick a date" v-model="ruleForm.date_of_physical_exam" :picker-options="pickerOptions" timezone="UTC"></el-date-picker>
-                    </el-form-item>
-                </el-form-item>
-                <el-form-item label="Place of Exam" prop="place_of_physical_exam">
-                    <el-input type="textarea" rows="2" v-model="ruleForm.place_of_physical_exam" :clearable="true"></el-input>
-                </el-form-item>
-                <el-form-item label="Type of Animal" prop="type_of_animal">
-                    <el-select v-model="ruleForm.type_of_animal" placeholder="Select" clearable>
-                        <el-option v-for="type in enumValues.type_of_animal" :key="type.code" :label="type.code" :value="type.code" :title="type.desc" />
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="Type of Expos." prop="type_of_exposure">
-                    <el-select v-model="ruleForm.type_of_exposure" placeholder="Select" clearable>
-                        <el-option v-for="type in enumValues.type_of_exposure" :key="type.code" :label="type.code" :value="type.code" :title="type.desc" />
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="Site of Infect." prop="site_of_infection">
-                    <el-select v-model="ruleForm.site_of_infection" multiple placeholder="Select one or more" style="width:100%" allow-create filterable default-first-option>
-                        <el-option v-for="type in enumValues.site_of_infection_history" :key="type.code" :label="type.code" :value="type.code" :title="type.desc" />
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="Washed ?" prop="is_washed">
-                    <el-select v-model="ruleForm.is_washed" placeholder="Select" clearable>
-                        <el-option v-for="type in enumValues.is_washed" :key="type.code" :label="type.desc" :value="type.code" :title="type.desc" />
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="Route" prop="route">
-                    <el-input v-model="ruleForm.route" clearable></el-input>
-                </el-form-item>
-                <el-form-item label="Category" prop="category">
-                    <el-select v-model="ruleForm.category" placeholder="Select" clearable>
-                        <el-option v-for="type in enumValues.category" :key="type.code" :label="type.code" :value="type.code" :title="type.desc" />
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="Outcome" prop="outcome">
-                    <el-select v-model="ruleForm.outcome" placeholder="Select" clearable>
-                        <el-option v-for="type in enumValues.outcome" :key="type.code" :label="type.code" :value="type.code" :title="type.desc" />
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="Animal Status" prop="bitting_animal_status">
-                    <el-select v-model="ruleForm.bitting_animal_status" placeholder="Select" clearable>
-                        <el-option v-for="type in enumValues.bitting_animal_status" :key="type.code" :label="type.code" :value="type.code" :title="type.desc" />
-                    </el-select>
-                </el-form-item>
+            <el-tab-pane label="Patient Exposure">
+                <!-- <el-row :gutter="10">
+                    <el-col :xs="12" :sm="12" :md="16" :lg="16" :xl="16"><div class="grid-content bg-purple"></div></el-col>
+                    <el-col :xs="12" :sm="12" :md="16" :lg="16" :xl="16"><div class="grid-content bg-purple-light"></div></el-col>
+                </el-row> -->
+                <el-row>
+                    <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+                        <el-form-item label="Date of Incident" required>
+                            <el-form-item prop="date_of_incident">
+                                <el-date-picker ref="name" type="date" style="width:100%" size="small" placeholder="Pick a date" v-model="ruleForm.date_of_incident" :picker-options="pickerOptions" timezone="UTC"></el-date-picker>
+                            </el-form-item>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+                        <el-form-item label="Place of Inci." prop="place_of_incident">
+                            <el-input type="textarea" rows="1" style="width:100%" v-model="ruleForm.place_of_incident" :clearable="true"></el-input>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+
+                <el-row>
+                    <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+                        <el-form-item label="Date of Exam" required>
+                            <el-form-item prop="date_of_physical_exam">
+                                <el-date-picker type="date" style="width:100%" size="small" placeholder="Pick a date" v-model="ruleForm.date_of_physical_exam" :picker-options="pickerOptions" timezone="UTC"></el-date-picker>
+                            </el-form-item>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+                        <el-form-item label="Place of Exam" prop="place_of_physical_exam">
+                            <el-input type="textarea" rows="1" style="width:100%" v-model="ruleForm.place_of_physical_exam" :clearable="true"></el-input>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+
+                <el-row>
+                    <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+                        <el-form-item label="Type of Animal" prop="type_of_animal">
+                            <el-select v-model="ruleForm.type_of_animal" placeholder="Select" style="width:100%" size="small" clearable>
+                                <el-option v-for="type in enumValues.type_of_animal" :key="type.code" :label="type.code" :value="type.code" :title="type.desc" />
+                            </el-select>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+                        <el-form-item label="Type of Expos." prop="type_of_exposure">
+                            <el-select v-model="ruleForm.type_of_exposure" placeholder="Select" style="width:100%" size="small" clearable>
+                                <el-option v-for="type in enumValues.type_of_exposure" :key="type.code" :label="type.code" :value="type.code" :title="type.desc" />
+                            </el-select>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+
+                <el-row>
+                    <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+                        <el-form-item label="Site of Infect." prop="site_of_infection">
+                            <el-select v-model="ruleForm.site_of_infection" multiple placeholder="Select one or more" style="width:100%" size="small" allow-create filterable default-first-option>
+                                <el-option v-for="type in enumValues.site_of_infection_history" :key="type.code" :label="type.code" :value="type.code" :title="type.desc" />
+                            </el-select>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :xs="24" :sm="24" :md="6" :lg="6" :xl="6">
+                        <el-form-item label="Washed ?" prop="is_washed">
+                            <el-select v-model="ruleForm.is_washed" placeholder="Select" style="width:100%" size="small" clearable>
+                                <el-option v-for="type in enumValues.is_washed" :key="type.code" :label="type.desc" :value="type.code" :title="type.desc" />
+                            </el-select>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :xs="24" :sm="24" :md="6" :lg="6" :xl="6">
+                        <el-form-item label="Route" prop="route">
+                            <el-input v-model="ruleForm.route" style="width:100%" size="small" clearable></el-input>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+
+                <el-row>
+                    <el-col :xs="24" :sm="24" :md="8" :lg="8" :xl="8">
+                        <el-form-item label="Category" prop="category">
+                            <el-select v-model="ruleForm.category" placeholder="Select" style="width:100%" size="small" clearable>
+                                <el-option v-for="type in enumValues.category" :key="type.code" :label="type.code" :value="type.code" :title="type.desc" />
+                            </el-select>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :xs="24" :sm="24" :md="8" :lg="8" :xl="8">
+                        <el-form-item label="Outcome" prop="outcome">
+                            <el-select v-model="ruleForm.outcome" placeholder="Select" style="width:100%" size="small" clearable>
+                                <el-option v-for="type in enumValues.outcome" :key="type.code" :label="type.code" :value="type.code" :title="type.desc" />
+                            </el-select>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :xs="24" :sm="24" :md="8" :lg="8" :xl="8">
+                        <el-form-item label="Animal Status" prop="bitting_animal_status">
+                            <el-select v-model="ruleForm.bitting_animal_status" placeholder="Select" style="width:100%" size="small" clearable>
+                                <el-option v-for="type in enumValues.bitting_animal_status" :key="type.code" :label="type.code" :value="type.code" :title="type.desc" />
+                            </el-select>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+
                 <el-form-item label="Remarks" prop="remarks">
-                    <el-input type="textarea" rows="6" v-model="ruleForm.remarks" :clearable="true"></el-input>
+                    <el-input type="textarea" rows="4" v-model="ruleForm.remarks" size="small" :clearable="true"></el-input>
                 </el-form-item>
                 <el-form-item align="right">
                     <el-button @click="resetForm('ruleForm')">Reset Field</el-button>
@@ -89,7 +132,7 @@
             </el-tab-pane>
             <el-tab-pane label="Patient Treatment Session">
                 <el-form-item align="right">
-                    <el-button type="primary">Add New</el-button>
+                    <el-button type="primary" @click="addTreatmentSession">Add New</el-button>
                 </el-form-item>
                 <div v-if="treatmentList.length">
                     <div v-for="t in treatmentList" :key="t.id">
@@ -246,6 +289,13 @@ export default {
         },
         rezize() {
             this.size = dialogSize(window.innerWidth)
+        },
+        addTreatmentSession() {
+            this.treatmentList.unshift({
+                designated_day: '',
+                date: '',
+                vaccine_id: ''
+            })
         }
     },
     created() {
