4772a8220233b7821f46c73a7a2156c16a36ba1f
mkjdy <markdy61@gmail.com>
2021-12-12 02:48:08 +0800

    change history migration

--- database/migrations/2021_07_04_073107_create_patient_histories_table.php
+++ database/migrations/2021_07_04_073107_create_patient_histories_table.php
@@ -14,20 +14,50 @@ class CreatePatientHistoriesTable extends Migration
     public function up()
     {
         Schema::create('patient_histories', function (Blueprint $table) {
+            // $table->id();
+            // $table->foreignId('patient_id')->constrained('patients');
+            // $table->dateTime('date_of_incident');
+            // $table->text('place_of_incident');
+            // $table->dateTime('date_of_physical_exam');
+            // $table->text('place_of_physical_exam');
+            // $table->enum('type_of_animal', ['PD', 'PC']);
+            // $table->enum('type_of_exposure', ['B', 'NB']);
+            // $table->json('site_of_infection')->nullable();
+            // $table->boolean('is_washed')->default(true);
+            // $table->string('route');
+            // $table->enum('category', ['1', '2', '3']);
+            // $table->enum('outcome', ['C', 'INC', 'N', 'D']);
+            // $table->enum('biting_animal_status', ['ALIVE', 'DEAD', 'LOST']);
+            // $table->text('remarks');
+            // $table->timestamps();
             $table->id();
             $table->foreignId('patient_id')->constrained('patients');
             $table->dateTime('date_of_incident');
             $table->text('place_of_incident');
             $table->dateTime('date_of_physical_exam');
             $table->text('place_of_physical_exam');
-            $table->enum('type_of_animal', ['PD', 'PC']);
-            $table->enum('type_of_exposure', ['B', 'NB']);
-            $table->json('site_of_infection')->nullable();
+
+            $table->unsignedBigInteger('type_of_animal_id');
+            $table->foreign('type_of_animal_id')->references('id')->on('references');
+
+            $table->unsignedBigInteger('type_of_exposure_id');
+            $table->foreign('type_of_exposure_id')->references('id')->on('references');
+
+            $table->json('site_of_infection_id')->nullable();
+
             $table->boolean('is_washed')->default(true);
+
             $table->string('route');
-            $table->enum('category', ['1', '2', '3']);
-            $table->enum('outcome', ['C', 'INC', 'N', 'D']);
-            $table->enum('biting_animal_status', ['ALIVE', 'DEAD', 'LOST']);
+
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('references');
+
+            $table->unsignedBigInteger('outcome_id');
+            $table->foreign('outcome_id')->references('id')->on('references');
+
+            $table->unsignedBigInteger('biting_animal_status_id');
+            $table->foreign('biting_animal_status_id')->references('id')->on('references');
+
             $table->text('remarks');
             $table->timestamps();
         });
