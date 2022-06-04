feb6bac925b3e6a5a01a23e8fb14736399150d03
mkjdy <markdy61@gmail.com>
2022-06-04 21:49:45 +0800

    queue event

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,11 @@ namespace App\Providers;
 // use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
+// use Illuminate\Support\Facades\Log;
+// use Illuminate\Support\Facades\Queue;
+// use Illuminate\Queue\Events\JobProcessed;
+// use Illuminate\Queue\Events\JobProcessing;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -28,5 +33,28 @@ class AppServiceProvider extends ServiceProvider
         // if (env('APP_ENV') !== 'local') {
         //     URL::forceScheme('https');
         // }
+
+        // Queue::before(function (JobProcessing $event) {
+
+        //     $payload = json_decode( $event->job->getRawBody() );
+        //     $data = unserialize( $payload->data->command );
+
+        //     info('Starting to process job', [
+        //             'data_here' => $data,
+        //           'connection' => $event->connectionName,
+        //           'job' => $event->job,
+        //           'payload' => $event->job->payload()
+        //     ]);
+        // });
+
+        // Queue::after(function (JobProcessed $event) {
+        //     info('Finished processing job', [
+        //           'connection' => $event->connectionName,
+        //           'job' => $event->job,
+        //           'payload' => $event->job->payload()
+        //     ]);
+        // });
+
+
     }
 }
